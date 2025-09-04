35a05585a4b97522679a95dbb4263ab880572805
Blustak <menditta1@gmail.com>
2025-09-04 11:31:02 +0100

    add:postgres integration

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 
 	"github.com/Blustak/bootdev-gator/internal/config"
+	"github.com/Blustak/bootdev-gator/internal/database"
 )
 
 type state struct {
+    db *database.Queries
     userConfig *config.Config
 }
 
@@ -34,7 +42,7 @@ func (c *commands) run(s *state, cmd command) error {
 func (c *commands) register(name string, f func(*state,command) error) {
     _,ok := c.cmd[name]
     if ok {
-        fmt.Println("warning: command %s is being overwritten", name)
+        fmt.Printf("warning: command %s is being overwritten\n", name)
     }
     c.cmd[name] = f
 }
@@ -45,10 +53,20 @@ func main() {
         fmt.Printf("error: %v\n", err)
     }
     currentState := state{ userConfig:&cfgFile}
+
+    db,err := sql.Open("postgres",cfgFile.DbUrl)
+    if err != nil {
+        fmt.Printf("error: %v", err)
+    }
+    currentState.db = database.New(db)
+
     cmds := commands {
         cmd: make(map[string]func(*state,command) error),
     }
     cmds.register("login", handlerLogin)
+    cmds.register("register",handlerRegister)
+    cmds.register("reset",handlerReset)
+    cmds.register("users",handlerUsers)
 
     if len(os.Args) < 2 {
         fmt.Println("error: not enough arguments")
@@ -69,8 +87,66 @@ func handlerLogin(s *state,cmd command) error {
     if len(cmd.args) == 0 {
         return fmt.Errorf("login requires a username")
     }
-    if err := s.userConfig.SetUser(cmd.args[0]); err != nil {
+    user,err := s.db.GetUser(
+        context.Background(),
+        cmd.args[0],
+    )
+    if err != nil {
         return err
     }
+    if err := s.userConfig.SetUser(user.Name); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func handlerRegister(s *state,cmd command) error {
+    if len(cmd.args) == 0 {
+        return fmt.Errorf("register requires a username")
+    }
+    nowTime := time.Now()
+    user,err := s.db.CreateUser(
+        context.Background(),
+        database.CreateUserParams{
+            ID: uuid.New(),
+            CreatedAt: nowTime,
+            UpdatedAt: nowTime,
+            Name: cmd.args[0],
+    })
+    if err != nil {
+        return err
+    }
+    if err := s.userConfig.SetUser(user.Name); err != nil {
+        return err
+    }
+    fmt.Printf("Created user %s\nData:%v\n",user.Name,user)
+    return nil
+
+
+}
+
+func handlerUsers(s *state, cmd command) error {
+    users, err := s.db.GetUsers(
+        context.Background(),
+    )
+    if err != nil {
+        return err
+    }
+    for _,u := range users {
+        if u.Name == s.userConfig.CurrentUserName {
+            fmt.Printf("* %s (current)\n",u.Name)
+        } else {
+            fmt.Printf("* %s\n", u.Name)
+        }
+    }
+    return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+    if err := s.db.Reset(context.Background()); err != nil {
+        return err
+    }
+    fmt.Println("successfully reset users table")
     return nil
 }
