122cd40878ed2e3c9b517199cb356cd4c52b1d40
Blustak <menditta1@gmail.com>
2025-09-05 12:54:50 +0100

    add:feed and feed following functionality

--- main.go
+++ main.go
@@ -12,141 +12,266 @@ import (
 
 	"github.com/Blustak/bootdev-gator/internal/config"
 	"github.com/Blustak/bootdev-gator/internal/database"
+	"github.com/Blustak/bootdev-gator/internal/rss"
 )
 
 type state struct {
-    db *database.Queries
-    userConfig *config.Config
+	db         *database.Queries
+	userConfig *config.Config
 }
 
 type command struct {
-    name string
-    args []string
+	name string
+	args []string
 }
 
 type commands struct {
-    cmd map[string]func(*state,command) error
+	cmd map[string]func(*state, command) error
 }
 
 func (c *commands) run(s *state, cmd command) error {
-    mappedCmd, ok := c.cmd[cmd.name]
-    if !ok {
-        return fmt.Errorf("command %s is not registered",cmd.name)
-    }
-    if err := mappedCmd(s,cmd); err != nil {
-        return err
-    }
-    return nil
+	mappedCmd, ok := c.cmd[cmd.name]
+	if !ok {
+		return fmt.Errorf("command %s is not registered", cmd.name)
+	}
+	if err := mappedCmd(s, cmd); err != nil {
+		return err
+	}
+	return nil
 }
 
-func (c *commands) register(name string, f func(*state,command) error) {
-    _,ok := c.cmd[name]
-    if ok {
-        fmt.Printf("warning: command %s is being overwritten\n", name)
-    }
-    c.cmd[name] = f
+func (c *commands) register(name string, f func(*state, command) error) {
+	_, ok := c.cmd[name]
+	if ok {
+		fmt.Printf("warning: command %s is being overwritten\n", name)
+	}
+	c.cmd[name] = f
 }
 
 func main() {
-    cfgFile, err := config.ReadUserConfig()
-    if err != nil {
-        fmt.Printf("error: %v\n", err)
-    }
-    currentState := state{ userConfig:&cfgFile}
+	cfgFile, err := config.ReadUserConfig()
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+	currentState := state{userConfig: &cfgFile}
 
-    db,err := sql.Open("postgres",cfgFile.DbUrl)
-    if err != nil {
-        fmt.Printf("error: %v", err)
-    }
-    currentState.db = database.New(db)
+	db, err := sql.Open("postgres", cfgFile.DbUrl)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+	currentState.db = database.New(db)
 
-    cmds := commands {
-        cmd: make(map[string]func(*state,command) error),
-    }
-    cmds.register("login", handlerLogin)
-    cmds.register("register",handlerRegister)
-    cmds.register("reset",handlerReset)
-    cmds.register("users",handlerUsers)
+	cmds := commands{
+		cmd: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerUsers)
+	cmds.register("agg", handlerAgg)
+	cmds.register("addfeed", handlerAddFeed)
+	cmds.register("feeds", handlerFeeds)
+	cmds.register("follow", handlerFollow)
+    cmds.register("following",handlerFollowing)
 
-    if len(os.Args) < 2 {
-        fmt.Println("error: not enough arguments")
-        os.Exit(1)
-    }
-    cmd := command{
-        name: os.Args[1],
-        args: os.Args[2:],
-    }
-    if err := cmds.run(&currentState,cmd); err != nil {
-        fmt.Printf("error: %v\n",err)
-        os.Exit(1)
-    }
+	if len(os.Args) < 2 {
+		fmt.Println("error: not enough arguments")
+		os.Exit(1)
+	}
+	cmd := command{
+		name: os.Args[1],
+		args: os.Args[2:],
+	}
+	if err := cmds.run(&currentState, cmd); err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
 
 }
 
-func handlerLogin(s *state,cmd command) error {
-    if len(cmd.args) == 0 {
-        return fmt.Errorf("login requires a username")
-    }
-    user,err := s.db.GetUser(
-        context.Background(),
-        cmd.args[0],
-    )
-    if err != nil {
-        return err
-    }
-    if err := s.userConfig.SetUser(user.Name); err != nil {
-        return err
-    }
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("login requires a username")
+	}
+	user, err := s.db.GetUser(
+		context.Background(),
+		cmd.args[0],
+	)
+	if err != nil {
+		return err
+	}
+	if err := s.userConfig.SetUser(user.Name); err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
-func handlerRegister(s *state,cmd command) error {
-    if len(cmd.args) == 0 {
-        return fmt.Errorf("register requires a username")
-    }
-    nowTime := time.Now()
-    user,err := s.db.CreateUser(
-        context.Background(),
-        database.CreateUserParams{
-            ID: uuid.New(),
-            CreatedAt: nowTime,
-            UpdatedAt: nowTime,
-            Name: cmd.args[0],
-    })
-    if err != nil {
-        return err
-    }
-    if err := s.userConfig.SetUser(user.Name); err != nil {
-        return err
-    }
-    fmt.Printf("Created user %s\nData:%v\n",user.Name,user)
-    return nil
-
+func handlerRegister(s *state, cmd command) error {
+	if err := checkArgs(1, cmd); err != nil {
+		return err
+	}
+	nowTime := time.Now()
+	user, err := s.db.CreateUser(
+		context.Background(),
+		database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: nowTime,
+			UpdatedAt: nowTime,
+			Name:      cmd.args[0],
+		})
+	if err != nil {
+		return err
+	}
+	if err := s.userConfig.SetUser(user.Name); err != nil {
+		return err
+	}
+	fmt.Printf("Created user %s\nData:%v\n", user.Name, user)
+	return nil
 
 }
 
 func handlerUsers(s *state, cmd command) error {
-    users, err := s.db.GetUsers(
-        context.Background(),
-    )
-    if err != nil {
-        return err
-    }
-    for _,u := range users {
-        if u.Name == s.userConfig.CurrentUserName {
-            fmt.Printf("* %s (current)\n",u.Name)
-        } else {
-            fmt.Printf("* %s\n", u.Name)
-        }
-    }
-    return nil
+	users, err := s.db.GetUsers(
+		context.Background(),
+	)
+	if err != nil {
+		return err
+	}
+	for _, u := range users {
+		if u.Name == s.userConfig.CurrentUserName {
+			fmt.Printf("* %s (current)\n", u.Name)
+		} else {
+			fmt.Printf("* %s\n", u.Name)
+		}
+	}
+	return nil
 }
 
 func handlerReset(s *state, cmd command) error {
-    if err := s.db.Reset(context.Background()); err != nil {
-        return err
-    }
-    fmt.Println("successfully reset users table")
+	if err := s.db.Reset(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println("successfully reset users table")
+	return nil
+}
+
+func handlerAgg(s *state, cmd command) error {
+	url := "https://www.wagslane.dev/index.xml"
+	feed, err := rss.NewFeed(context.Background(), url)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%v\n", feed)
+	return nil
+
+}
+
+func handlerAddFeed(s *state, cmd command) error {
+	if err := checkArgs(2, cmd); err != nil {
+		return err
+	}
+	current_user, err := s.db.GetUser(context.Background(), s.userConfig.CurrentUserName)
+	if err != nil {
+		return err
+	}
+	nowTime := time.Now()
+	feed, err := s.db.AddFeed(context.Background(), database.AddFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: nowTime,
+		UpdatedAt: nowTime,
+		Name:      cmd.args[0],
+		Url:       cmd.args[1],
+		UserID:    current_user.ID,
+	})
+	if err != nil {
+		return err
+	}
+    s.db.CreateFeedFollow(context.Background(),database.CreateFeedFollowParams{
+        ID: uuid.New(),
+        CreatedAt: nowTime,
+        UpdatedAt: nowTime,
+        UserID: current_user.ID,
+        FeedID: feed.ID,
+
+    })
+	fmt.Printf("%v\n", feed)
+	return nil
+
+}
+
+func handlerFeeds(s *state, cmd command) error {
+	if err := checkArgs(0, cmd); err != nil {
+		return err
+	}
+	feeds, err := s.db.GetAllFeeds(context.Background())
+	if err != nil {
+		return err
+	}
+	for _, feed := range feeds {
+		user, err := s.db.GetUserByID(context.Background(), feed.UserID)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Feed %v:\n", feed.ID)
+		fmt.Printf("\tName: %s\n", feed.Name)
+		fmt.Printf("\tURL: %s\n", feed.Url)
+		fmt.Printf("\tUsername: %s\n", user.Name)
+	}
+	return nil
+}
+
+func handlerFollow(s *state, cmd command) error {
+	if err := checkArgs(1, cmd); err != nil {
+		return err
+	}
+	feed, err := s.db.GetFeedByUrl(context.Background(), cmd.args[0])
+	if err != nil {
+		return err
+	}
+	nowTime := time.Now()
+	currentUser, err := getCurrentUser(s)
+	if err != nil {
+		return err
+	}
+	s.db.CreateFeedFollow(context.Background(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: nowTime,
+		UpdatedAt: nowTime,
+		UserID:    currentUser.ID,
+		FeedID:    feed.ID,
+	})
+	fmt.Printf("Feed name: %s\nFeed URL:%s\n", feed.Name, feed.Url)
+	return nil
+}
+
+func handlerFollowing(s *state, cmd command) error {
+	user, err := getCurrentUser(s)
+	if err != nil {
+		return err
+	}
+	res, err := s.db.GetFeedFollowsForUser(context.Background(), user.ID)
+	if err != nil {
+		return err
+	}
+    fmt.Printf("User %s is currently following:\n",user.Name)
+	for _, feed := range res {
+        fmt.Printf("\t - %s\n",feed.FeedName)
+	}
     return nil
+}
+
+func checkArgs(expectedLength int, cmd command) error {
+	if len(cmd.args) != expectedLength {
+		return fmt.Errorf("%s expected %d args; got %d.", cmd.name, expectedLength, len(cmd.args))
+	}
+	return nil
+}
+
+func getCurrentUser(s *state) (*database.User, error) {
+	user, err := s.db.GetUser(context.Background(), s.userConfig.CurrentUserName)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
---
+++ sql/queries/feed_follows.sql
@@ -0,0 +1,29 @@
+-- name: CreateFeedFollow :many
+
+WITH cte_feedFollows AS (
+    INSERT INTO feed_follows(id,created_at,updated_at,user_id,feed_id)
+    VALUES (
+        $1,
+        $2,
+        $3,
+        $4,
+        $5
+    ) RETURNING *
+) SELECT
+    cte_feedFollows.*,
+    feeds.name AS feed_name,
+    users.name AS user_name
+FROM cte_feedFollows
+INNER JOIN users ON users.id = cte_feedFollows.user_id
+INNER JOIN feeds ON feeds.id = cte_feedFollows.feed_id;
+
+-- name: GetFeedFollowsForUser :many
+
+SELECT 
+    feed_follows.*,
+    users.name AS user_name,
+    feeds.name AS feed_name
+FROM feed_follows
+INNER JOIN users ON users.id = feed_follows.user_id
+INNER JOIN feeds ON feeds.id = feed_follows.feed_id
+WHERE feed_follows.user_id = $1;
